e044b2b0c956d8d6d2aad13e931f2d448f4f138e
Chris Janusiewicz <krzysztof.janusiewicz@proton.ch>
2024-05-09 13:27:26 +0200

    feat(persistence): pull Persistence into tvOS app

--- libraries/Shared/Localization/Package.swift
+++ libraries/Shared/Localization/Package.swift
@@ -1,11 +1,15 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Localization",
-    platforms: [.iOS(.v15), .macOS(.v11)],
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v11),
+        .tvOS(.v17)
+    ],
     products: [
         .library(
             name: "Localization",
